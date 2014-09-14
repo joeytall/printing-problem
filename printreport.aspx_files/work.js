b1fcdf5926ae3111b77bc4ec08c07eaf97000199
joeytall <joeytall.liu@gmail.com>
2014-09-14 10:55:44 -0700

    init push
    Change-Id: Iad9f64f340981aec0a8841a87fee54e4dac07e5b

---
+++ printreport.aspx_files/work.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  // $(".rgMasterTable").removeClass("rgMasterTable");
+  // $(".rgClipCells").removeClass("rgClipCells");
+  // $(".RadGrid").removeClass("RadGrid");
+  // $(".rgRow").removeClass("rgRow");
+  // $(".rgAltRow").removeClass("rgAltRow");
+  setTimeout(setobjects("0"), 0);
+})
