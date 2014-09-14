f5fdcedfa5f299c1c9eee371a03144c2733f7526
joeytall <joeytall.liu@gmail.com>
2014-09-14 13:32:38 -0700

    works on my machine
    Change-Id: I8413d6fa316d6d51b8a588a2a55010e8f7c0b2c1

--- printreport.aspx_files/work.js
+++ printreport.aspx_files/work.js
@@ -5,4 +5,25 @@ $(document).ready(function () {
   // $(".rgRow").removeClass("rgRow");
   // $(".rgAltRow").removeClass("rgAltRow");
   setTimeout(setobjects("0"), 0);
+
+  $(".RadGrid").each(function(){
+    var pageHeight = 900,
+    gridTop = parseInt($(this).css("top")),
+    gridHeight = $(this).height();
+  if (gridTop % pageHeight + gridHeight > pageHeight)
+  {
+    movedown($(this).prev());
+    movedown($(this).next());
+  }
+  });
 })
+
+function movedown(obj){
+  var marginTop = 25,
+      originalTop = parseInt(obj.css("top")),
+      newTop = originalTop + marginTop,
+      nextObj = obj.next();
+  obj.css("top",newTop);
+  if(nextObj.length != 0)
+    movedown(nextObj);
+}
